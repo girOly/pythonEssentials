65b2b65f5dcafa2825f25eedebd146977959e4bd
girOly <girardob@gmail.com>
2020-01-18 13:41:00 -0500

    weightCOnverter project

--- projects/weight_converter.py
+++ projects/weight_converter.py
@@ -5,16 +5,27 @@
 # Weight: 72
 # (L)bs or (K)g:
 # You are 160lbs
+#  BMI = kg/m2
+
 
 user_weight = input('How much do you weigh: ')
-user_settings = input("(L)bs or (K)gs ")
+user_settingw = input("(L)bs or (K)gs ")
+user_height = input('How tall are you: ')
+user_settingh = input("(M)eters or (F)eets ")
 
-lowercase_input = user_settings.lower()
+lowercase_inputw = user_settingw.lower()
+lowercase_inputh = user_settingh.lower()
 
-
-if lowercase_input == "l":
-    user_weight_kgs = int(user_weight) * 2.2
-    print("You are : " + str(user_weight_kgs) + " lbs")
-if lowercase_input == "k":
+if lowercase_inputw == "l":
     user_weight_lbs = int(user_weight) * 0.45
     print("You are : " + str(user_weight_lbs) + " kgs")
+if lowercase_inputw == "k":
+    user_weight_kgs = float(user_weight) * 2.2
+    print("You are : " + str(user_weight_kgs) + " lbs")
+
+if lowercase_inputh == "m":
+    user_height_f = float(user_height) * 3.28
+    print("You are : " + str(user_height_f) + " ft")
+if lowercase_inputh == "f":
+    user_height_m = float(user_height) / 3.28
+    print("You are : " + str(user_height_m) + " m")
