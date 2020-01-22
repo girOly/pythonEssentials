1c1a5395bfaadf46390463dac5cca6a3c8a5e152
girOly <girardob@gmail.com>
2020-01-22 14:46:06 -0500

    objects

--- fundamentals/dictionaries.py
+++ fundamentals/dictionaries.py
@@ -7,3 +7,28 @@ customer = {
     "age": 30,
     "is_verified": True
 }
+
+customer["name"] = "Jack Smith"
+print(customer.get('birthday', "Jan 1 1980"))
+print(customer["name"])
+
+customer["birthday"] = 'January 1st'
+
+phone = input("Phone: ")
+
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eigth",
+    "9": "Nine"
+}
+output = ""
+for char in phone:
+    output += digits_mapping.get(char, "!") + " "
+
+print(output)
