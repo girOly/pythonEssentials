b9968883f9401ca31e343c507a14c600db82710f
girOly <girardob@gmail.com>
2020-01-23 15:25:51 -0500

    restful routes

--- restful_flask/first_api/app.py
+++ restful_flask/first_api/app.py
@@ -1,11 +1,74 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 9.99
+            }
+        ]
+    }
+]
+# @app.route('/')
+# def home():
+#     return "Hello World"
 
-@app.route('/')
-def home():
-    return "Hello World"
+# Post - Used to receive data
+# Get - Used to send data back only
+
+# Post /store data: { name: }
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+# Get  /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+
+# Get /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# Post /store/<string:name>/item: {name:, price:}
+@app.route('/store/<string:name>/item')
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+        return jsonify({'message': 'store not found'})
+
+        # Get /store/<string:name>/item
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+        return jsonify({'message': 'Items not found'})
 
 
 app.run(port=5000)
